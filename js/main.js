9a129c4d147281558783d22ada491a428d16028a
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-06-23 21:23:12 +0300

    save

--- js/main.js
+++ js/main.js
@@ -6,5 +6,19 @@ $(function(){
         showinfo : 0
     },
    
+   });
+   
+    $('.heroes__slider-img').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.heroes__slider-text',
+       prevArrow: '<button class="slick-btn slick-prev"><img src="./images/arrow-l.png" alt="prev"></button>'
+  
+});
+$('.heroes__slider-text').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+   asNavFor: '.heroes__slider-img',
+  fade: true,
 });
 });
